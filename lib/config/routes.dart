071b62b813cdfa52cd7da097793ccc58390aa656
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2025-05-25 22:48:46 +0900

    Feat: 방만들기 페이지 추가

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -2,6 +2,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tayo_fe/screens/mypage/mypage_screen.dart';
+import '../screens/create/carpool_create_room_screen.dart';
+import '../screens/create/fixed_carpool_create_room_screen.dart';
+import '../screens/create/taxi_create_room_screen.dart';
 import '../screens/login/login_screen.dart';
 import '../screens/home/home_screen.dart';
 import '../screens/login/signin_screen.dart';
@@ -34,6 +37,19 @@ class AppRouter {
           ),
         ],
       ),
+      GoRoute(
+        path: '/create-carpool',
+        builder: (context, state) => const CarpoolCreateRoomScreen(),
+      ),
+      GoRoute(
+        path: '/create-taxi',
+        builder: (context, state) => const TaxiCreateRoomScreen(),
+      ),
+      GoRoute(
+        path: '/create-fixed-carpool',
+        builder: (context, state) => const FixedCarpoolCreateRoomScreen(),
+      ),
+
     ],
     errorBuilder:
         (context, state) =>
